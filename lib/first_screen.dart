a4e13d2e457adf5efc8bf6f82ce9bf8b6f17b5b3
zj <zj2000x@mail.mail>
2020-03-11 22:48:56 +0200

    handle back button

--- lib/first_screen.dart
+++ lib/first_screen.dart
@@ -5,27 +5,53 @@ import 'package:flutter/material.dart';
 class FirstScreen extends StatelessWidget {
   @override
   build(BuildContext ctx) {
-    return Scaffold(
-      body: Container(
-        color: Colors.blue[700],
-        child: Center(
-          child: Button(
-            'Go to second screen',
-            onPressed: () async {
-              final result =  await Navigator.of(ctx).pushNamed(Routes.secondScreen);
+    return WillPopScope(
+      onWillPop: () {
+        return showDialog(
+            context: ctx,
+            builder: (_) {
+              return AlertDialog(
+                title: Text('Confirm exit app'),
+                actions: [
+                  MaterialButton(
+                    child: Text('No'),
+                    onPressed: () => Navigator.of(ctx).pop(),
+                  ),
+                  MaterialButton(
+                    child: Text('Yes'),
+                    onPressed: () => Navigator.of(ctx).pop(),
+                  ),
+                ],
+              );
+            });
+      },
+      child: Scaffold(
+        body: Container(
+          color: Colors.blue[700],
+          child: Center(
+            child: Button(
+              'Go to second screen',
+              onPressed: () async {
+                final result =
+                    await Navigator.of(ctx).pushNamed(Routes.secondScreen);
 
-                showDialog(context: ctx, builder: (_) {
-                  return AlertDialog(
-                    title: Text(result.toString()),
-                    actions: [
-                      MaterialButton(
-                        child: Text('Dismiss'),
-                        onPressed: () => Navigator.of(ctx).pop(),
-                      ),
-                    ],
-                  );
-                });
-            },
+                if (result != null) {
+                  showDialog(
+                      context: ctx,
+                      builder: (_) {
+                        return AlertDialog(
+                          title: Text(result.toString()),
+                          actions: [
+                            MaterialButton(
+                              child: Text('Dismiss'),
+                              onPressed: () => Navigator.of(ctx).pop(),
+                            ),
+                          ],
+                        );
+                      });
+                }
+              },
+            ),
           ),
         ),
       ),
