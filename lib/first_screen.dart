01acfb369dc1b426616aa8f7a7bc11e63e52841b
zj <zj2000x@mail.mail>
2020-03-19 09:10:57 +0200

    added http, move onButtonTap to separate method

--- lib/first_screen.dart
+++ lib/first_screen.dart
@@ -13,26 +13,7 @@ class FirstScreen extends StatelessWidget {
           child: Center(
             child: Button(
               'Go to second screen',
-              onPressed: () async {
-                final result =
-                    await Navigator.of(ctx).pushNamed(Routes.secondScreen);
-
-                if (result != null) {
-                  showDialog(
-                      context: ctx,
-                      builder: (_) {
-                        return AlertDialog(
-                          title: Text(result.toString()),
-                          actions: [
-                            MaterialButton(
-                              child: Text('Dismiss'),
-                              onPressed: () => Navigator.of(ctx).pop(),
-                            ),
-                          ],
-                        );
-                      });
-                }
-              },
+              onPressed: () => _onTapNextScreenButton(ctx),
             ),
           ),
         ),
@@ -40,23 +21,45 @@ class FirstScreen extends StatelessWidget {
     );
   }
 
-  Future<bool> _onBackButton(BuildContext ctx) {
-    return showDialog(
+  Future<void> _onTapNextScreenButton(BuildContext ctx) async {
+    final result = await Navigator.of(ctx).pushNamed(Routes.secondScreen);
+
+    if (result != null) {
+      showDialog(
         context: ctx,
         builder: (_) {
           return AlertDialog(
-            title: Text('Confirm exit app'),
+            title: Text(result.toString()),
             actions: [
               MaterialButton(
-                child: Text('No'),
-                onPressed: () => Navigator.of(ctx).pop(),
-              ),
-              MaterialButton(
-                child: Text('Yes'),
+                child: Text('Dismiss'),
                 onPressed: () => Navigator.of(ctx).pop(),
               ),
             ],
           );
-        });
+        },
+      );
+    }
+  }
+
+  Future<bool> _onBackButton(BuildContext ctx) {
+    return showDialog(
+      context: ctx,
+      builder: (_) {
+        return AlertDialog(
+          title: Text('Confirm exit app'),
+          actions: [
+            MaterialButton(
+              child: Text('No'),
+              onPressed: () => Navigator.of(ctx).pop(),
+            ),
+            MaterialButton(
+              child: Text('Yes'),
+              onPressed: () => Navigator.of(ctx).pop(),
+            ),
+          ],
+        );
+      },
+    );
   }
 }
